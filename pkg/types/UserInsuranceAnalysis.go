a2479dca9e0300295b81e3d0aeeff8f51ac2bc0b
Victor <hello@victordias.dev>
2022-08-21 16:55:08 -0300

    conforming name to standarts

--- pkg/types/UserInsuranceAnalysis.go
+++
@@ -1,8 +0,0 @@
-package types
-
-type UserInsuranceAnalysis struct {
-	Vehicle    string `json:"auto"`
-	Disability string `json:"disability"`
-	Home       string `json:"home"`
-	Life       string `json:"life"`
-}
