3865861c0495ffbc25b0fa6b57b363788ca4f2aa
Victor <hello@victordias.dev>
2022-08-21 18:28:44 -0300

    creating an pipeline of risk steps and an engine to run it

---
+++ pkg/riskEngine/riskEngine.go
@@ -0,0 +1,32 @@
+package riskengine
+
+import (
+	"github.com/victormagalhaess/origin-backend-take-home-assignment/pkg/riskSteps"
+	"github.com/victormagalhaess/origin-backend-take-home-assignment/pkg/types"
+)
+
+var Pipeline = riskSteps.NewPipeline()
+
+func InitializePipeline() {
+	Pipeline.AddSteps(
+		riskSteps.Over60,
+		riskSteps.Under40,
+		riskSteps.Under30,
+		riskSteps.NoHouse,
+		riskSteps.MortgagedHouse,
+		riskSteps.NoIncome,
+		riskSteps.IncomeOver200k,
+		riskSteps.Married,
+		riskSteps.Dependents,
+		riskSteps.NoVehicle,
+		riskSteps.VehicleBuiltLast5Years,
+	)
+}
+
+func ExecutePipeline(userInfo types.UserPersonalInformation,
+	insuranceSteps *types.UserInsuranceAnalysisSteps) {
+	for _, step := range Pipeline.RiskSteps {
+		step(userInfo, insuranceSteps)
+	}
+
+}
---
+++ pkg/riskSteps/stepsPipeline.go
@@ -0,0 +1,25 @@
+package riskSteps
+
+import "github.com/victormagalhaess/origin-backend-take-home-assignment/pkg/types"
+
+type Step func(userInfo types.UserPersonalInformation, insuranceSteps *types.UserInsuranceAnalysisSteps)
+type RiskPipeline struct {
+	RiskSteps []Step
+}
+
+func (pipeline *RiskPipeline) AddStep(step Step) {
+	pipeline.RiskSteps = append(pipeline.RiskSteps, step)
+}
+
+func (pipeline *RiskPipeline) AddSteps(steps ...Step) {
+	pipeline.RiskSteps = append(pipeline.RiskSteps, steps...)
+}
+
+// In order to add a new risk step, add a new step function in the riskSteps package and add it to the pipeline
+// in the riskEngine package.
+func NewPipeline() RiskPipeline {
+	return RiskPipeline{
+		RiskSteps: []Step{},
+	}
+
+}
