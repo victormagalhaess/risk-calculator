58718b70c62c8f7808699065f9ee79db648e3847
Victor <hello@victordias.dev>
2022-08-22 08:51:24 -0300

    restructuring the pipeline package

--- pkg/riskEngine/riskEngine.go
+++ pkg/riskEngine/riskEngine.go
@@ -1,25 +1,26 @@
 package riskEngine
 
 import (
-	"github.com/victormagalhaess/origin-backend-take-home-assignment/pkg/riskSteps"
+	"github.com/victormagalhaess/origin-backend-take-home-assignment/pkg/pipeline"
+	"github.com/victormagalhaess/origin-backend-take-home-assignment/pkg/pipeline/steps"
 	"github.com/victormagalhaess/origin-backend-take-home-assignment/pkg/types"
 )
 
-var Pipeline = riskSteps.NewPipeline()
+var Pipeline = pipeline.NewPipeline()
 
 func InitializePipeline() {
 	Pipeline.AddSteps(
-		riskSteps.Over60,
-		riskSteps.Under40,
-		riskSteps.Under30,
-		riskSteps.NoHouse,
-		riskSteps.MortgagedHouse,
-		riskSteps.NoIncome,
-		riskSteps.IncomeOver200k,
-		riskSteps.Married,
-		riskSteps.Dependents,
-		riskSteps.NoVehicle,
-		riskSteps.VehicleBuiltLast5Years,
+		steps.Over60,
+		steps.Under40,
+		steps.Under30,
+		steps.NoHouse,
+		steps.MortgagedHouse,
+		steps.NoIncome,
+		steps.IncomeOver200k,
+		steps.Married,
+		steps.Dependents,
+		steps.NoVehicle,
+		steps.VehicleBuiltLast5Years,
 	)
 }
 
