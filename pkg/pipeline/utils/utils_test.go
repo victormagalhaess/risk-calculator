36fbacf4e2f57341317ed205c4cda59cc0c2795a
Victor <hello@victordias.dev>
2023-11-01 09:00:00 -0300

    restructuring the pipeline/utils in its own package

---
+++ pkg/pipeline/utils/utils_test.go
@@ -0,0 +1,81 @@
+package utils_test
+
+import (
+	"testing"
+
+	"github.com/victormagalhaess/origin-backend-take-home-assignment/pkg/pipeline/utils"
+	"github.com/victormagalhaess/origin-backend-take-home-assignment/pkg/types"
+)
+
+func TestAddToAllValues(t *testing.T) {
+	t.Run("Positive Increment", func(t *testing.T) {
+		insuranceSteps := types.UserInsuranceAnalysisSteps{
+			Life: types.StepResult{
+				Risk:        0,
+				Eligibility: true,
+			},
+			Disability: types.StepResult{
+				Risk:        0,
+				Eligibility: true,
+			},
+			Auto: types.StepResult{
+				Risk:        0,
+				Eligibility: true,
+			},
+			Home: types.StepResult{
+				Risk:        0,
+				Eligibility: true,
+			},
+		}
+
+		utils.AddToAllValues(&insuranceSteps, 1)
+		if insuranceSteps.Life.Risk != 1 {
+			t.Errorf("Life.Risk is not 1")
+		}
+		if insuranceSteps.Disability.Risk != 1 {
+			t.Errorf("Disability.Risk is not 1")
+		}
+		if insuranceSteps.Auto.Risk != 1 {
+			t.Errorf("Auto.Risk is not 1")
+		}
+		if insuranceSteps.Home.Risk != 1 {
+			t.Errorf("Home.Risk is not 1")
+		}
+	})
+
+	t.Run("Negative Increment", func(t *testing.T) {
+		insuranceSteps := types.UserInsuranceAnalysisSteps{
+			Life: types.StepResult{
+				Risk:        0,
+				Eligibility: true,
+			},
+			Disability: types.StepResult{
+				Risk:        0,
+				Eligibility: true,
+			},
+			Auto: types.StepResult{
+				Risk:        0,
+				Eligibility: true,
+			},
+			Home: types.StepResult{
+				Risk:        0,
+				Eligibility: true,
+			},
+		}
+
+		utils.AddToAllValues(&insuranceSteps, -2)
+		if insuranceSteps.Life.Risk != -2 {
+			t.Errorf("Life.Risk is not -2")
+		}
+		if insuranceSteps.Disability.Risk != -2 {
+			t.Errorf("Disability.Risk is not -2")
+		}
+		if insuranceSteps.Auto.Risk != -2 {
+			t.Errorf("Auto.Risk is not -2")
+		}
+		if insuranceSteps.Home.Risk != -2 {
+			t.Errorf("Home.Risk is not -2")
+		}
+	})
+
+}
